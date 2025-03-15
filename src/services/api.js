a4fb947790482f66e9b88c92b81d9e34e0fe4ca3
Private ? <raylyreyes1@gmail.com>
2025-03-15 16:50:39 +0800

    Merge branch 'main' of https://github.com/Jeyseh/Brigada-Pagbasa-Frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://52.231.93.214:5000/api";
 
 export const loginUser = async (email, password) => {
     try {
